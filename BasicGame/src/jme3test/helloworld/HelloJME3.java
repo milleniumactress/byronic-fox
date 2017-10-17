7194aa4ea90c402de666a5ff099948ddd7a9473f
Muhammad Ravi <muhammadravi04@gmail.com>
2017-10-17 11:45:26 +0700

    Add files via upload
    tutorial JMonkey

---
+++ BasicGame/src/jme3test/helloworld/HelloJME3.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jme3test.helloworld;
+
+/**
+ *
+ * @author Ravi
+ */
+
+import com.jme3.app.SimpleApplication;
+import com.jme3.material.Material;
+import com.jme3.math.Vector3f;
+import com.jme3.scene.Geometry;
+import com.jme3.scene.shape.Box;
+import com.jme3.math.ColorRGBA;
+
+/** Sample 1 - how to get started with the most simple JME 3 application.
+ * Display a blue 3D cube and view from all sides by
+ * moving the mouse and pressing the WASD keys.
+ * initialize the scene
+ */
+public class HelloJME3 extends SimpleApplication {
+
+    public static void main(String[] args){
+        HelloJME3 app = new HelloJME3();//instaniate your game
+        app.start(); // start the game//start your game
+    }
+
+    @Override
+    public void simpleInitApp() {
+        Box b = new Box(2, 2, 2); // create cube shape
+        Geometry geom = new Geometry("Box", b);  // create cube geometry from the shape
+        Material mat = new Material(assetManager,
+          "Common/MatDefs/Misc/Unshaded.j3md");  // create a simple material
+        mat.setColor("Color", ColorRGBA.Red);   // set color of material to blue
+        geom.setMaterial(mat);                   // set the cube's material
+        rootNode.attachChild(geom);              // make the cube appear in the scene
+    }
+}

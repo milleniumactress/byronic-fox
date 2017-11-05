ace8b7539ceb12e0d059210b0d99c8e79e2e1951
Muhammad Ravi <muhammadravi04@gmail.com>
2017-11-05 10:54:39 +0700

    Add files via upload

---
+++ Test/src/mygame/Tester.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mygame;
+
+/**
+ *
+ * @author Ravi
+ */
+public class Tester {
+
+    public static void main(String[] args) {
+        HelloCollision app = new HelloCollision();
+        app.start();
+    }
+}
